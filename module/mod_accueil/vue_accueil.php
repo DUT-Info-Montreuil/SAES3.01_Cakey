60d9b5982fcea420d130ff388b12f3f5a4dde747
huseyinC1876 <huseyinc1876@gmail.com>
2024-01-10 16:36:04 +0100

    header, footer dans vue generique, creation du mofule mod_accueil, transformation la page d'accueil html en php

---
+++ module/mod_accueil/vue_accueil.php
@@ -0,0 +1,51 @@
+<?php class VueAccueil extends VueGenerique{
+
+public function __contruct(){
+    parent::__construct();
+}
+
+public function page(){
+    self::header();
+    echo ' 
+    <html>
+        <head>
+            <title>Page</title>
+            <meta charset="utf-8">
+            <!--<link rel="stylesheet" href="style.css"> -->
+            <link rel="stylesheet" href="css/bootstrap.css">        
+        </head>
+        <body>
+            <div class ="histoire">
+                <div class="imgText">
+                    <img src ="Ressources/coeur.png">
+                    <p>Nouveautés</p>
+                </div>
+                <h3>L\'univers et l\'histoire de Cakey</h3>
+                </br>
+                </br>
+                </br>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+                Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus 
+                mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+            </div>
+            <div class="gameplayVideo">
+                <video width="640" height="480" controls>
+                    <source src="Ressources/gameplayGakey.ogx" type="video/ogg"> 
+                </video>
+            </div>
+            <!--IL FAUT ENLEVER LES </BR> ET REGLER LE PROBLEME DE LA VIDEO MAL ALLIGNER-->
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+            </br>
+        </body>
+    </html>';
+    self::footer();
+    
+}
+
+}
--- vueGenerique.php
+++ vueGenerique.php
@@ -9,7 +9,7 @@
     }
 
     public function header(){
-        echo'
+        echo '
         <html>
             <head>
                 <title>Page</title>
@@ -21,10 +21,10 @@
                 <nav>
                     <ul class ="menu">
                         <li class="logo" href="accueil.html">
-                            <a href="accueil.html"><img src="Ressources/gateaux.png"/>CAKEY</a>
+                            <a href="index.php?getmodule=modAccueil&action=page"><img src="Ressources/gateaux.png"/>CAKEY</a>
                         </li>
                         <li>
-                            <a href ="accueil.html">Accueil</a>
+                            <a href ="index.php?getmodule=modAccueil&action=page">Accueil</a>
                         </li>
                         <li class="ongletUnivers"><a href="#">Univers &ensp;</a>
                             <ul class="sousOngletsUnivers">
@@ -42,15 +42,44 @@
                             <img src="https://images-na.ssl-images-amazon.com/images/I/41gYkruZM2L.png" alt="search icon" class="button" />
                         </li>
                         <li>
-                            <a href ="#">Inscription</a>
+                            <a href ="index.php?getmodule=modConnexion&action=formulaireInscription">Inscription</a>
                         </li>
                         <li>
-                            <a href ="#">Connexion</a>
+                            <a href ="index.php?getmodule=modConnexion&action=formulaireConnexion">Connexion</a>
                         </li>
                     </ul>
                 </nav>
-        </html>
-        '               
+            </body>    
+        </html>';               
+    }
+
+    public function footer(){
+        echo '
+        <html>
+            <body>
+                <footer>
+                <ul class="footer">
+                    <li><a href = "#">Autres jeux de Cakey Group</a></li>
+                    <li><a href = "#">Team</a></li>
+                    <li><a href = "#">Terms</a></li>
+                    <li><a href = "#">Privacy</a></li>
+                    <li><a href = "#">Cookies</a></li>
+                </ul>
+                <ul class="icon">
+                    <li><a href="#"><img src="Ressources/facebookIcon.png"></a></li>
+                    <li><a href="#"><img src="Ressources/twitterIcon.png"></a></li>
+                    <li><a href="#"><img src="Ressources/InstagramIcon.png"></a></li>
+                </ul>
+            </footer>
+
+                <!--<script src="js/bootstrap.js"></script>-->
+                <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+        </body>
+    </html>
+        ';
     }
 
 }
